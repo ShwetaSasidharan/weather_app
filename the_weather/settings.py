bdc4c63797009527cea359ada104e74de3ca9f3f
Shweta Sasidharan <shweta.sasidharan@gmail.com>
2020-07-20 14:26:21 +1000

    A

--- the_weather/settings.py
+++ the_weather/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'q!ke37#w9koij#uu%)knoc#lwj9p*i)93c-rgy^5e8g4uur%u3'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['weather-ss.herokuapp.com','127.0.0.1']
 
 
 # Application definition
